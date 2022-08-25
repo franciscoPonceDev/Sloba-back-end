58f03e43cd90f68ff30c6c5029adb6f8a85c8e76
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-08-25 14:18:09 -0300

    some changes to authentication

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_182923) do
     t.string "title", null: false
     t.string "description", null: false
     t.datetime "end_date"
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
